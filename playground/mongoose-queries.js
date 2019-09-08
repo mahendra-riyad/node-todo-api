b1bed6e57c024702ff49175b2ade9578e9cc1287
mahe-ndra <mahendrariyad88@gmail.com>
2019-09-08 17:00:11 +0530

    id validation

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,40 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+var id = '5d606a903034b96031ea9637';
+
+if (!ObjectID.isValid(id)) {
+  console.log('ID not valid');
+}
+
+Todo.find({
+  _id: id
+}).then((todos) => {
+  console.log('Todos', todos);
+});
+
+Todo.findOne({
+  _id: id
+}).then((todo) => {
+  console.log('Todo', todo);
+});
+
+Todo.findById(id).then((todo) => {
+  if (!todo) {
+    return console.log('Id not found');
+  }
+  console.log('Todo By Id', JSON.stringify(todo, undefined, 2));
+}).catch((e) => console.log(e));
+
+User.findById('5d606a903034b96031ea9637').then((user) => {
+  if (!user) {
+    return console.log('Unable to find user');
+  }
+
+  console.log(JSON.stringify(user, undefined, 2));
+}, (e) => {
+  console.log(e);
+});
